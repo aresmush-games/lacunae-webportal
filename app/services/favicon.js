c9fd997892d823630f72d08439e4ea0a23ac22ae
julia <julia@polyforest.com>
2019-08-19 14:49:52 -0700

    Add favicons | Resolves #3

--- app/services/favicon.js
+++ app/services/favicon.js
@@ -3,9 +3,9 @@ import Service from '@ember/service';
 export default Service.extend({
     
   changeFavicon(active) {
-        var src = '/game/uploads/theme_images/favicon.ico';
+        var src = '/game/uploads/misc/favicon.ico';
         if (active) { 
-            src = '/game/uploads/theme_images/favicon-active.ico';
+            src = '/game/uploads/misc/favicon-active.ico';
         }
         $('link[rel="shortcut icon"]').attr('href', src);
         
